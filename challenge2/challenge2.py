a1fb20c11dc5bf1aa9db6e19f2d842a09b5df4d2
Chevi Sutton <chevi@redskytech.io>
2019-09-10 22:37:21 -0600

    Close with challenge two

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -15,7 +15,15 @@ class Challenge2(unittest.TestCase):
         title = self.driver.title
         print(" - Title:", title)
         self.assertEqual(title, self.driver.title)
-        self.assertTrue(title == self.driver.title)
+        # self.assertTrue(title == self.driver.title)
+        search_input = self.driver.find_element_by_css_selector('#input-search')
+        search_input.send_keys('Porsche')
+        search_btn = self.driver.find_element_by_css_selector('.btn-lightblue')
+        search_btn.click()
+        # exotics_page = self.driver.('#serverSideDataTable_wrapper')
+        # self.assertEqual(exotics_page, self.driver.find_element_by_css_selector('#serverSideDataTable_wrapper'))
+
+
 
 
 if __name__ == '__main__':

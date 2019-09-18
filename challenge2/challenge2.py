9dcc5a1b9a9fdc42af18565b641e13874851fbb0
Chevi Sutton <chevi@redskytech.io>
2019-09-17 22:42:43 -0600

    challenge to is passing

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -3,10 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as cond
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import TimeoutException
 
-porsche = "Porsche"
+porsche = "PORSCHE"
 
 
 class Challenge2(unittest.TestCase):
@@ -27,13 +25,11 @@ class Challenge2(unittest.TestCase):
         search_input.send_keys(porsche)
         print(" - Keys Sent:", porsche)
         search_btn = self.driver.find_element_by_css_selector('.btn-lightblue')
-        WebDriverWait(search_btn, 10)
         search_btn.click()
-        element = self.driver.find_element_by_css_selector(".search-resultstable")
-        WebDriverWait(element, 10)
-        # exotics_page = self.driver.find_element_by_class_name('search-resultstable')
-        # WebDriverWait(self.driver, 10).until(exotics_page)
-    # self.assertEqual(exotics_page, self.driver.find_element_by_css_selector('#serverSideDataTable_wrapper'))
+        porsche_currently_listed = WebDriverWait(self.driver, 10).until(
+            cond.visibility_of_element_located((By.XPATH, "//table[@id='serverSideDataTable']/tbody/tr[1]/td[5]")))
+        print(" - Text:", porsche_currently_listed.text)
+        self.assertTrue(porsche_currently_listed.text == porsche)
 
 
 if __name__ == '__main__':
